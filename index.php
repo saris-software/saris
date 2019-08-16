e129be52a40ed518c7fd10f11d5e33c34ca267fe
mpundidanford <danfordmpundi069@gmail.com>
2019-08-16 15:29:26 +0300

    changes

--- index.php
+++ index.php
@@ -6,11 +6,8 @@
 	require_once('Connections/zalongwa.php');
 	#Get Organisation Name
 	$qorg = "SELECT * FROM organisation";
-<<<<<<< HEAD
-	$dborg = mysqli_query($qorg);
-=======
+//	$dborg = mysqli_query($qorg);
 	$dborg = mysqli_query($zalongwa, $qorg);
->>>>>>> 80d9e2142351871928501cd59c77a1fcb3d4ab4b
 	$row_org = mysqli_fetch_assoc($dborg);
 	$org = $row_org['Name'];
 	$post = $row_org['Address'];
