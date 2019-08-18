5ed54c31bf78e169b8c46fa9b14436bdcebee4d7
Medydullah <medydullah01@gmail.com>
2019-08-18 08:59:30 -0400

    lecturerSemister update

--- academic/lecturerSemesterUpdate.php
+++ academic/lecturerSemesterUpdate.php
@@ -18,30 +18,30 @@
 $currentPage = $_SERVER["PHP_SELF"];
 
 //populate academic year combo box
-mysql_select_db($database_zalongwa, $zalongwa);
+mysqli_select_db($zalongwa, $database_zalongwa);
 $query_AYear = "SELECT AYear FROM academicyear ORDER BY AYear DESC";
-$AYear = mysql_query($query_AYear, $zalongwa) or die(mysql_error());
-$row_AYear = mysql_fetch_assoc($AYear);
-$totalRows_AYear = mysql_num_rows($AYear);
+$AYear = mysqli_query($zalongwa, $query_AYear) or die(mysqli_error());
+$row_AYear = mysqli_fetch_assoc($AYear);
+$totalRows_AYear = mysqli_num_rows($AYear);
 
 //populate semester combo box
-mysql_select_db($database_zalongwa, $zalongwa);
+mysqli_select_db($database_zalongwa, $zalongwa);
 $query_sem = "SELECT Semester FROM terms ORDER BY Semester ASC LIMIT 2";
-$sem = mysql_query($query_sem, $zalongwa) or die(mysql_error());
-$row_sem = mysql_fetch_assoc($sem);
-$totalRows_sem = mysql_num_rows($sem);
+$sem = mysqli_query($zalongwa, $query_sem) or die(mysqli_error());
+$row_sem = mysqli_fetch_assoc($sem);
+$totalRows_sem = mysqli_num_rows($sem);
 
 //populate coursecode combo box
-mysql_select_db($database_zalongwa, $zalongwa);
+mysqli_select_db($zalongwa, $database_zalongwa);
 if($privilege == 3){
 	$query_course = "SELECT DISTINCT CourseCode FROM examregister WHERE RegNo='$username' ORDER BY CourseCode ASC";	
 	}
 else{
 	$query_course = "SELECT CourseCode FROM course ORDER BY CourseCode ASC";
 	}
-$course = mysql_query($query_course, $zalongwa) or die(mysql_error());
-$row_course = mysql_fetch_assoc($course);
-$totalRows_course = mysql_num_rows($course);
+$course = mysqli_query($zalongwa, $query_course) or die(mysqli_error());
+$row_course = mysqli_fetch_assoc($course);
+$totalRows_course = mysqli_num_rows($course);
 
 if(isset($_POST['confirm']) && ($_POST['confirm']=='Confirm')){
 
@@ -59,8 +59,8 @@ $pageNum_ExamOfficerGradeBook = 0;
 $query = "UPDATE examregister SET Semester='$sem' WHERE CourseCode ='$key' AND AYear = '$ayear'";
 $qupdadateexamresult = "UPDATE examresult SET Semester='$sem' WHERE CourseCode ='$key' AND AYear = '$ayear'";
 
-$result = mysql_query($query);
-$result1 = mysql_query($qupdadateexamresult);
+$result = mysqli_query($zalongwa, $query);
+$result1 = mysqli_query($qupdadateexamresult);
 
 	if($result) {
 	echo " Database Updated Successful!";
@@ -81,10 +81,10 @@ do {
 <option value="<?php echo $row_AYear['AYear']?>"><?php echo $row_AYear['AYear']?></option>
                         <?php
 } while ($row_AYear = mysql_fetch_assoc($AYear));
-  $rows = mysql_num_rows($AYear);
+  $rows = mysqli_num_rows($AYear);
   if($rows > 0) {
-      mysql_data_seek($AYear, 0);
-	  $row_AYear = mysql_fetch_assoc($AYear);
+      mysqli_data_seek($AYear, 0);
+	  $row_AYear = mysqli_fetch_assoc($AYear);
   }
 ?>
       </select></td>
@@ -98,11 +98,11 @@ do {
 ?>
  <option value="<?php echo $row_sem['Semester']?>"><?php echo $row_sem['Semester']?></option>
                         <?php
-} while ($row_sem = mysql_fetch_assoc($sem));
-  $rows = mysql_num_rows($sem);
+} while ($row_sem = mysqli_fetch_assoc($sem));
+  $rows = mysqli_num_rows($sem);
   if($rows > 0) {
-      mysql_data_seek($sem, 0);
-	  $row_sem = mysql_fetch_assoc($sem);
+      mysqli_data_seek($sem, 0);
+	  $row_sem = mysqli_fetch_assoc($sem);
   }
 ?>
             </select></td>
@@ -116,11 +116,11 @@ do {
 ?>
                         <option value="<?php echo $row_course['CourseCode']?>"><?php echo $row_course['CourseCode']?></option>
                         <?php
-} while ($row_course = mysql_fetch_assoc($course));
-  $rows = mysql_num_rows($course);
+} while ($row_course = mysqli_fetch_assoc($course));
+  $rows = mysqli_num_rows($course);
   if($rows > 0) {
-      mysql_data_seek($course, 0);
-	  $row_course = mysql_fetch_assoc($course);
+      mysqli_data_seek($course, 0);
+	  $row_course = mysqli_fetch_assoc($course);
   }
 ?>
             </select></td>
@@ -134,6 +134,6 @@ do {
 </form>
 <?php
 }
-@mysql_free_result(@$ExamOfficerGradeBook);
+@mysqli_free_result(@$ExamOfficerGradeBook);
 include('../footer/footer.php');
 ?>
