0e715b1c3b48e113b16b2278fd5f1422c78e550c
Medydullah <medydullah01@gmail.com>
2019-08-18 07:30:32 -0400

    lectureHeader

--- Connections/sessioncontrol.php
+++ Connections/sessioncontrol.php
@@ -1,11 +1,13 @@
 <?php
+session_start();
 session_cache_limiter('nocache');
 @$module  = $_SESSION['module'];
 @$privilege  = $_SESSION['privilege'];
 @$RegNo = $_SESSION['RegNo'];
 @$username = $_SESSION['username'];
 @$name = $_SESSION['loginName'];
-@$userFaculty = $_SESSION['userFaculty']; 
+@$userFaculty = $_SESSION['userFaculty'];
+
 date_default_timezone_set('Africa/Dar_es_Salaam');
 $today = date("Y-m-d");
 
@@ -14,5 +16,4 @@ if(!$username){
 	echo '<meta http-equiv = "refresh" content ="0; url = ../index.php">';
 	exit;
 }
-session_start();
 ?>
