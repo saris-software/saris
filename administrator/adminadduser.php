8697130f24ca0130fa472c9cccfd6b620b25b42b
mpundidanford <danfordmpundi069@gmail.com>
2019-08-19 10:48:51 +0300

    changes in adminadduser.php

--- administrator/adminadduser.php
+++ administrator/adminadduser.php
@@ -233,9 +233,9 @@ if (fmAdd.txtLastName.value === "" || fmAdd.txtFirstName.value === "" || fmAdd.s
                                             style="font-family: Verdana, Arial, Helvetica, sans-serif; font-size: x-small; color: #000000; ">
 
 <label>
-<INPUT TYPE="text" SIZE="29" name="txtLastName" value="<?php echo((isset($_POST["txtLastName"])))?$_POST["txtLastName"]??  ?>">
+<INPUT TYPE="text" SIZE="29" name="txtLastName" value="<?php echo((isset($_POST["txtLastName"]))?$_POST["txtLastName"]:"") ?>">
 </label>
-</span><span style="color: rgb (000000); "><span class="large style4"><span style="color: #0000CC; ">(LASTNAME)</span></span> </span></div></TD>
+</span><span style="color: rgb(000000); "><span class="large style4"><span style="color: #0000CC; ">(LASTNAME)</span></span> </span></div></TD>
                             
                           </TR>
                           <TR>
