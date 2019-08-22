367ba95229b9a770f067608e52240173bd533a1d
Muneersahel <saidmunir73@gmail.com>
2019-08-22 21:37:59 +0300

    added some changes

--- student/admissionRegistration.php
+++ student/admissionRegistration.php
@@ -1,19 +1,19 @@
-<?php 
-	require_once('../Connections/sessioncontrol.php');
-	# include the header
-	include('studentMenu.php');
-	global $szSection, $szSubSection, $szTitle, $additionalStyleSheet;
-	$szSection = 'Admission Process';
-	$szTitle = 'Student Registration';
-	$szSubSection = '';
-	include("studentheader.php");
-	
+<?php
+require_once('../Connections/sessioncontrol.php');
+# include the header
+include('studentMenu.php');
+global $szSection, $szSubSection, $szTitle, $additionalStyleSheet;
+$szSection = 'Admission Process';
+$szTitle = 'Student Registration';
+$szSubSection = '';
+include("studentheader.php");
+
 ?>
-<p><br><br><em>Please Fill the <font color="#C11B17">Regisration Form</font> provided 
-<br> Make sure you provide the <font color="#C11B17">CORRECT INFORMATION</font></em></p>
+<p><br><br><em>Please Fill the <span style="color: #C11B17; ">Regisration Form</span> provided
+        <br> Make sure you provide the <span style="color: #C11B17; ">CORRECT INFORMATION</span></em></p>
 <br><br>
 <?php
 
-	# include the footer
-	include("../footer/footer.php");
+# include the footer
+include("../footer/footer.php");
 ?>
